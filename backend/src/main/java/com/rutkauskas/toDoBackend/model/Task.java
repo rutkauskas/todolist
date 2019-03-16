f41941dbefef495cce451f47d7789c7ca8f946e3
Mindaugas Rutkauskas <jabzore@gmail.com>
2019-03-16 12:23:27 +0200

    Added constructor and get/set methods

--- backend/src/main/java/com/rutkauskas/toDoBackend/model/Task.java
+++ backend/src/main/java/com/rutkauskas/toDoBackend/model/Task.java
@@ -1,5 +1,32 @@
 package com.rutkauskas.toDoBackend.model;
 
+import java.util.UUID;
+
 public class Task {
 
+  private UUID id;
+  private String title;
+  private boolean archived;
+
+  public Task(UUID id, String title, boolean archived) {
+    this.id = id;
+    this.title = title;
+    this.archived = archived;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean isArchived() {
+    return archived;
+  }
+
+  public void setArchived(boolean archived) {
+    this.archived = archived;
+  }
 }
