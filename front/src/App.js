2081583a42890d012104bfb71a9f5bc94e24af29
Mindaugas Rutkauskas <jabzore@gmail.com>
2019-03-16 16:53:05 +0200

    Added functionality to Submit new todo button

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -9,21 +10,31 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Todo Task 1',
         archived: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Todo Task 2',
         archived: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Todo Task 3',
         archived: false
       }
     ]
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      archived: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+    console.log(title)
   }
 
   render() {
@@ -31,7 +42,7 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos}/>
         </div>
       </div>
