918aa3a6c23b01647573559514df00a05b1112d8
Mindaugas Rutkauskas <jabzore@gmail.com>
2019-03-16 15:19:58 +0200

    Created fake todos and passing them down to Todos and TodoItem

--- front/src/App.js
+++ front/src/App.js
@@ -4,10 +4,30 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Todo Task 1',
+        archived: false
+      },
+      {
+        id: 2,
+        title: 'Todo Task 2',
+        archived: false
+      },
+      {
+        id: 3,
+        title: 'Todo Task 3',
+        archived: false
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
